2ca71aa94a2af7fb62d8b4fd48279527ac38b0ff
tanabenatsuki <railgun799@gmail.com>
2020-01-05 18:20:24 +0900

    deploy to heroku

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,10 +3,16 @@
 @section('content')
 <form method="POST" action="/posts{{ $post->id }}">
   {{ csrf_field() }}
-  <input type="hidden" name="_method" value="PUT">
-  <input type="text" name="title" value="{{ $post->title }}">
-  <input type="text" name="content" value="{{ $post->content }}">
-  <input type="submit">
+<input type="hidden" name="_method" value="PUT">
+  <div class="form-group">
+   <label for="exampleInputEmail1">Title</label>
+   <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ old('title')=='' ? $post->title:old('title') }}">
+  </div>
+  <div class="form-group">
+   <label for="exampleInputEmail1">Title</label>
+   <textarea class="form-control" name="content">{{ old('content') == '' ? $post->content : old('content') }}</textarea>
+  </div>
+  <button type="submit" class="btn btn-outline-primary">Submit</button>
 </form>
 @endsection
 
